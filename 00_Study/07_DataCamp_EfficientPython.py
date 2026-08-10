5fcc2d87a2f4a02b93ac762986dbc1896005a989
Cesar Alejandro Solano Suarez (DHL Supply Chain) <112773974+cesardro@users.noreply.github.com>
2026-08-10 13:12:00 +0200

    Update 07_DataCamp_EfficientPython.py

--- 00_Study/07_DataCamp_EfficientPython.py
+++ 00_Study/07_DataCamp_EfficientPython.py
@@ -732,11 +732,13 @@ print(baseball_df.head())
 4  CHC     NL  2012  613  759  61  162         0  0.38
 '''
 
-# ********************** ALTOGETHER ********************** 
+# ********************** ALTOGETHER **********************
+
 
 def predict_win_perc(RS, RA):
     prediction = RS ** 2 / (RS ** 2 + RA ** 2)
     return np.round(prediction, 2)
+
 
 win_perc_preds_loop = []
 
@@ -749,11 +751,13 @@ for row in baseball_df.itertuples():
 baseball_df['WP_loop'] = win_perc_preds_loop
 
 # Apply predict_win_perc to each row of the DataFrame
-win_perc_preds_apply = baseball_df.apply(lambda row: predict_win_perc(row['RS'], row['RA']), axis=1)
+win_perc_preds_apply = baseball_df.apply(
+    lambda row: predict_win_perc(row['RS'], row['RA']), axis=1)
 baseball_df['WP_apply'] = win_perc_preds_apply
 
 # Calculate the win percentage predictions using NumPy arrays
-win_perc_preds_np = predict_win_perc(baseball_df['RS'].values, baseball_df['RA'].values)
+win_perc_preds_np = predict_win_perc(
+    baseball_df['RS'].values, baseball_df['RA'].values)
 baseball_df['WP_preds'] = win_perc_preds_np
 print(baseball_df.head())
 
